3232129ec0dc444644f4c84cb1f4fb92214794eb
BimaAfka14 <bimaafkanur@gmail.com>
2025-05-12 03:39:45 +0700

    update vercel and cronjob

--- routes/cleanUpRoute.js
+++ routes/cleanUpRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const cleanUpHandler = require("../models/cleanUpHandler");
 
-router.post("/", cleanUpHandler);
+router.get("/", cleanUpHandler);
 
 module.exports = router;
