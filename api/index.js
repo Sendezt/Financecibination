24e84c3f5c6b8534c445936e263a72b47864d1b0
BimaAfka14 <bimaafkanur@gmail.com>
2025-05-21 02:29:58 +0700

    fix: correct app import path in serverless configuration

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 const serverless = require("serverless-http");
-const app = require("../app"); // pastikan path sesuai dengan app.js
+const app = require("../server"); // pastikan path sesuai dengan app.js
 
 module.exports = serverless(app);
