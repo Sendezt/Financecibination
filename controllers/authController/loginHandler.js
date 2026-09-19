5912de60cc7a14d800f9d64ca36b6a4e2d3bfef9
BimaAfka14 <bimaafkanur@gmail.com>
2026-09-19 19:52:44 +0700

    feat(auth): update SameSite attribute for JWT cookie to 'none' in production

--- controllers/authController/loginHandler.js
+++ controllers/authController/loginHandler.js
@@ -58,7 +58,7 @@ const loginHandler = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: isProduction,
-      sameSite: isProduction ? "strict" : "lax",
+      sameSite: isProduction ? "none" : "lax",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 hari dalam ms
     });
